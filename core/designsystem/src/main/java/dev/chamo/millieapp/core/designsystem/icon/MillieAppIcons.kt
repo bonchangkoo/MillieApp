8f8b4e63734c5f598941618ca442af74066f8b42
bonchangkoo <bonchang.koo@wesang.com>
2024-10-13 03:43:45 +0900

    MillieAppIcons 구현

---
+++ core/designsystem/src/main/java/dev/chamo/millieapp/core/designsystem/icon/MillieAppIcons.kt
@@ -0,0 +1,8 @@
+package dev.chamo.millieapp.core.designsystem.icon
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+
+object MillieAppIcons {
+    val ArrowBack = Icons.AutoMirrored.Rounded.ArrowBack
+}
