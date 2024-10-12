e702f99641c83f741d56baf5864a5edc5dc59991
bonchangkoo <bonchang.koo@wesang.com>
2024-10-12 23:02:43 +0900

    TopHeadline external 모델 구현

---
+++ core/model/src/main/kotlin/dev/chamo/millieapp/core/model/TopHeadLine.kt
@@ -0,0 +1,12 @@
+package dev.chamo.millieapp.core.model
+
+data class TopHeadLine(
+    val sourceName: String,
+    val author: String,
+    val title: String,
+    val description: String,
+    val url: String,
+    val urlToImage: String,
+    val publishedAt: String,
+    val content: String
+)
